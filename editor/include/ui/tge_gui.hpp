a55766b8664b190c1563ad012d19c7d805a57cf6
ntropy83 <oliver.sagner@pandorawrks.de>
2023-10-12 15:57:22 +0200

    started implementing imgui

--- editor/include/ui/tge_gui.hpp
+++ editor/include/ui/tge_gui.hpp
@@ -4,8 +4,6 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_vulkan.h"
 
-#include "vulkan/tge_device.hpp"
-
 namespace tge {
   class TgeEditorUI {
     public:
@@ -16,6 +14,17 @@ namespace tge {
       TgeEditorUI(const TgeEditorUI &) = delete;
 			TgeEditorUI &operator=(const TgeEditorUI &) = delete;
 
+      void imgui_init();
+
     private:
+      GLFWwindow          *window;
+      VkInstance&         g_Instance;
+      VkPhysicalDevice&   g_PhysicalDevice;
+      VkDevice&           g_Device;
+      VkQueueFamily       g_QueueFamily;
+      VkQueue             g_Queue;
+      VkPipelineCache     g_PipelineCache;
+      VkDescriptorPool    g_DescriptorPool;
+      Allocator           g_Allocator;
   };
 }
--- editor/src/ui/tge_gui.cpp
+++ editor/src/ui/tge_gui.cpp
@@ -2,4 +2,33 @@
 
 namespace tge {
 
+  void TgeEditorUI::imgui_init(){
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForVulkan(window, true);
+    ImGui_ImplVulkan_InitInfo init_info = {};
+    init_info.Instance = g_Instance;
+    init_info.PhysicalDevice = g_PhysicalDevice;
+    init_info.Device = g_Device;
+    init_info.QueueFamily = g_QueueFamily;
+    init_info.Queue = g_Queue;
+    init_info.PipelineCache = g_PipelineCache;
+    init_info.DescriptorPool = g_DescriptorPool;
+    init_info.Allocator = nullptr;
+    init_info.MinImageCount = g_MinImageCount;
+    init_info.ImageCount = wd->ImageCount;
+    init_info.CheckVkResultFn = check_vk_result;
+    ImGui_ImplVulkan_Init(&init_info, wd->RenderPass);
+  }
+
 }  
